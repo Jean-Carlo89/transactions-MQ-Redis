6d99c4052d320f08401e68552843d4550472fca3
Jean Carlo <jeancarlodev@gmail.com>
2024-11-29 17:22:38 -0300

    feat: add docker compose files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { config as dotenvConfig } from 'dotenv';
 
+dotenvConfig({ path: `.env.${process.env.NODE_ENV}` });
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nest',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
