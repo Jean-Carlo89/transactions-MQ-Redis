c7c07294470cab6c4842ede594322696d162b9e8
Jean Carlo <jeancarlodev@gmail.com>
2024-11-29 20:22:47 -0300

    feat: update .env file. Fix docker compose db connection issues

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,11 +11,12 @@ dotenvConfig({ path: `.env.${process.env.NODE_ENV}` });
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'nest',
+
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [Recharge],
       synchronize: true,
     }),
